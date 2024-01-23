cc64961b6638639a24bfe208b7ebb66f8fac25dc
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-01-23 20:01:54 +0300

    Feat:Added first lines of functionality

--- zipper/src/main.rs
+++ zipper/src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("Hello, world!");
-}
+extern crate flate2;
+use flate2::Compression;
+use flate2::write::GzEncoder;
+use flate2::read::GzDecoder;
+use std::io;
+use std::env::Args;
