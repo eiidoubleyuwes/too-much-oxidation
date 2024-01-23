534b94587fbfba2f17524d068bd7eba582bcb69e
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-01-23 20:22:04 +0300

    Feat:Code cleanup

--- zipper/src/main.rs
+++ zipper/src/main.rs
@@ -2,7 +2,7 @@ extern crate flate2;
 use flate2::Compression;
 use flate2::write::GzEncoder;
 use std::io::copy;
-use std::env::{args};
+use std::env::args;
 use std::fs::File;
 use std::io::BufReader;
 use std::time::Instant;
