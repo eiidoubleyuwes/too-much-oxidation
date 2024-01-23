4d1176420499c0b62717b90529a4b8219cd17e10
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-01-23 20:10:07 +0300

    Feat:Added Encoder functionality

--- zipper/src/main.rs
+++ zipper/src/main.rs
@@ -16,5 +16,9 @@ fn main() {
    }
    let mut input = BufReader::new(File::open(args().nth(1).unwrap()).unwrap());
    let output = File::create(args().nth(2).unwrap()).unwrap();
+   let mut encoder = GzEncoder::new(output, Compression::default());
+   let start = Instant::now();
+   copy(&mut input, &mut encoder).unwrap();
+   encoder.finish().unwrap();
 
 }
