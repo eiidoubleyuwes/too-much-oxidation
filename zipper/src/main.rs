d0e7e8321df416b5e02964daef9518c30e58b058
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-01-23 20:08:59 +0300

    Feat:Added ability to read files and this is somehow a bit of functionality

--- zipper/src/main.rs
+++ zipper/src/main.rs
@@ -2,5 +2,19 @@ extern crate flate2;
 use flate2::Compression;
 use flate2::write::GzEncoder;
 use flate2::read::GzDecoder;
-use std::io;
-use std::env::Args;
+use std::io::copy;
+use std::env::{args};
+use std::fs::File;
+use std::io::BufReader;
+use std::time::Instant;
+
+fn main() {
+    //Expect three items Usage,source and target
+   if args().len() != 3 {
+      println!("Usage: `source` `target`");
+      return;
+   }
+   let mut input = BufReader::new(File::open(args().nth(1).unwrap()).unwrap());
+   let output = File::create(args().nth(2).unwrap()).unwrap();
+
+}
