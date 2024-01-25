7f0b20635803f1bbf88dede3b66ee6dedd1c46a5
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-01-25 15:12:57 +0300

    Feat:Added a bit more code

--- no-zipper/src/main.rs
+++ no-zipper/src/main.rs
@@ -1,3 +1,12 @@
 //this program is for unzipping a file
 //It is meant to be complimentary to the one I made to zip files
-use std::BufReader;
+extern crate flate2;
+use std::io::BufReader;
+use std::io::prelude::*;
+use std::fs::File;
+use std::env;
+use flate2::Decompress;
+use flate2::write::GzEncoder;
+fn main(){
+    
+}
