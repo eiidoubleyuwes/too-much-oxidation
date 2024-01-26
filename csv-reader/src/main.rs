e9823eb5281a0063c115c58bd4b7f3b89aa8be65
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-01-27 00:33:41 +0300

    Feat:Bug introduction

--- csv-reader/src/main.rs
+++ csv-reader/src/main.rs
@@ -12,6 +12,18 @@ fn reader() -> i32{
         println!("Not enough arguments Usage: Filename");
         return 1;
     }
+    #[derive(Debug, Deserialize)]
+    struct Record{
+        name: String,
+        place: String,
+        id: String,
+    }
+    println!("Reading file {}",args[1]);
+    let mut rdr = csv::Reader::from_reader(fs::File::open(&args[1]).unwrap());
+    for result in rdr.deserialize(){
+        let record: Record = result.unwrap();
+        println!("{:?}",record);
+    }
     let filename = &args[1];
     let file = fs::File::open(filename).unwrap();
     let mut rdr = csv::Reader::from_reader(file);
