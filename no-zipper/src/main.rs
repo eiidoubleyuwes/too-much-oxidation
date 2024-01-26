8e6d3e94b8043bc47d7a140ac5f7dbe980c5be3e
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-01-26 17:52:06 +0300

    Feat:More functionality

--- no-zipper/src/main.rs
+++ no-zipper/src/main.rs
@@ -2,6 +2,7 @@
 //It is meant to be complimentary to the one I made to zip files
 use std::fs;
 use std::io;
+use std::iter::zip;
 fn main(){
     std::process::exit(unzip())
 }
@@ -14,4 +15,18 @@ fn unzip() -> i32{
         return 1;
     }
     let fname =std::path::Path::new(&*args[1]);
+    let file = fs::File::open(fname).unwrap();
+    //use zip archive
+    let mut archive = zip::ZipArchive::new(file).unwrap();
+    //for each file in the archive
+    for i in 0..archive.len(){
+        //get the file
+        let mut file = archive.by_index(i).unwrap();
+        //get the name of the file
+        let fname = file.name();
+        //create the file
+        let mut outfile = fs::File::create(fname).unwrap();
+        //copy the file
+        io::copy(&mut file, &mut outfile).unwrap();
+    }
 }
