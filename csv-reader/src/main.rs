917c07f1c204fb58a6509dd1d32ab2e70132d49f
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-01-27 00:26:18 +0300

    Feat:Added dependancies

--- csv-reader/src/main.rs
+++ csv-reader/src/main.rs
@@ -1,3 +1,9 @@
+//This tools should be able to read data from a csv file and print it out to the console
+//ofc the csv file should be in the same directory as the executable
+extern crate serde;
+use std::fs;
+use std::io;
+use serde::{Deserialize, Serialize};
 fn main() {
     println!("Hello, world!");
 }
