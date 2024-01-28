02fca2643e1bfd5a48d0b6b55c7e698489b5fa64
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-01-28 21:22:08 +0300

    Feat:Added structs

--- json-reader/src/main.rs
+++ json-reader/src/main.rs
@@ -1,5 +1,18 @@
 //Today I'm making a project that alllows me to read json files and print them out in a nice format.
 //I'm going to use serde_json to do this.
+use serde::{Deserialize, Serialize};
+#[derive(Serialize, Deserialize)]
+struct Sentensi {
+    name: String,
+    age: u8,
+    phones: Vec<String>,
+}
+#[derive(Serialize, Deserialize)]
+struct Article{
+    paragraphs: Vec<String>,
+    author: String,
+    author: String,
+}
 fn main() {
     println!("Hello,welsome to JSON Reader!");
     println!("Please enter the file name you want to read(Usage : File name): ");
