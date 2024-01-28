10c4ff0eedc4e5326e0a882b959e2e1b8e2581a2
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-01-28 21:15:44 +0300

    Feat:Added depenancies

--- json-reader/src/main.rs
+++ json-reader/src/main.rs
@@ -1,3 +1,7 @@
+//Today I'm making a project that alllows me to read json files and print them out in a nice format.
+//I'm going to use serde_json to do this.
 fn main() {
-    println!("Hello, world!");
+    println!("Hello,welsome to JSON Reader!");
+    println!("Please enter the file name you want to read(Usage : File name): ");
+    
 }
