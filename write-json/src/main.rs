10c7616ef3900275d4256cbcb83f3d7f85beebda
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-01-29 20:04:28 +0300

    Feat:Added structs

--- write-json/src/main.rs
+++ write-json/src/main.rs
@@ -1,5 +1,19 @@
 //This project is a simple rust project for me to learn how to write JSON files in rust.
 //I will be using the serde_json crate to do this.
+//I will also be using the serde crate to serialize the data.
+
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+struct Paragraph {
+    name: String,
+    
+}
+struct Article {
+   article = String,
+   author = String,
+   paragraph = vec<Paragraph>,
+}
 
 fn main() {
     println!("Hello, world!");
