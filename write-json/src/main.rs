385fd430a64b14b9cbaffc2dbfa32ceb72438e45
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-01-29 19:52:49 +0300

    Feat:Added dependancies

--- write-json/src/main.rs
+++ write-json/src/main.rs
@@ -1,3 +1,6 @@
+//This project is a simple rust project for me to learn how to write JSON files in rust.
+//I will be using the serde_json crate to do this.
+
 fn main() {
     println!("Hello, world!");
 }
