7e4d31dcafcc577d3abcca860181e8957c4aee9c
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-01-29 20:42:35 +0300

    Feat:Added ability for user to put their input
    This took longer than I would love it

--- cash-CS50-problem/src/main.rs
+++ cash-CS50-problem/src/main.rs
@@ -17,17 +17,19 @@ fn calc_total(pennies: i32, dimes: i32, quaters: i32, nickels: i32) -> i32 {
     calc_pennies(pennies) + calc_dimes(dimes) + calc_quaters(quaters) + calc_nickels(nickels)
 }
 fn main() {
-    let cents = 1000;
+    //The user should be able to give an input
+    println!("Enter the amount of cents");
+    let mut cents = String::new();
+    std::io::stdin().read_line(&mut cents).expect("Failed to read line");
+    let cents: i32 = cents.trim().parse().expect("Please type a number!");
     let pennies = calc_pennies(cents);
     let dimes = calc_dimes(cents);  
     let quaters = calc_quaters(cents);
     let nickels = calc_nickels(cents);
     let total = calc_total(pennies, dimes, quaters, nickels);
-    println!("Total is {}", total);
     println!("Pennies is {}", pennies);
     println!("Dimes is {}", dimes);
     println!("Quaters is {}", quaters);
     println!("Nickels is {}", nickels);
-
-
+    println!("Total is {}", total);
 }
