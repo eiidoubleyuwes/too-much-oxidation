4ecb172dbf3dd04ca40e31d8170772a08f974f69
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-01-30 21:14:24 +0300

    Feat:Added ability for user input

--- fizz-buzz/src/main.rs
+++ fizz-buzz/src/main.rs
@@ -1,3 +1,7 @@
+//This is a simple take on the FizzBuzz problem
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to fizz buzz,enter a number,to make it simple keep it between 1 and 100");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Bad input");
 }
