074474d7113af1bae3a5f5e23ef1c4a70cad8944
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-01-30 21:15:52 +0300

    Feat:Added final code

--- fizz-buzz/src/main.rs
+++ fizz-buzz/src/main.rs
@@ -8,6 +8,12 @@ fn main() {
     for i in 1..input {
         if i % 3 == 0 && i % 5 == 0 {
             println!("FizzBuzz");
-        
+        } else if i % 3 == 0 {
+            println!("Fizz");
+        } else if i % 5 == 0 {
+            println!("Buzz");
+        } else {
+            println!("{}", i);
+        }
     }
 }
