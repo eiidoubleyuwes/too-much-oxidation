020db97c23195703e87f8f4ad3d41c931d5e0c3a
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-02-01 17:42:35 +0300

    Feat:Added first lines

--- get-request/src/main.rs
+++ get-request/src/main.rs
@@ -4,8 +4,7 @@ use std::io::Read;
 error_chain 
 
 fn main() {
-    let mut file = std::fs::File::open("foo.txt").unwrap();
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
-    println!("{}", contents);
+    let mut request = reqwest::get("https://httpbin.org/ip")?
+        .expect("Couldn't make request");
+    
 }
