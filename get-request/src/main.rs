750e2ab42154d369ac5e713bc6fb74270869c239
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-02-01 17:40:48 +0300

    Feat:Added more functionality

--- get-request/src/main.rs
+++ get-request/src/main.rs
@@ -3,3 +3,9 @@ use std::io::Read;
 
 error_chain 
 
+fn main() {
+    let mut file = std::fs::File::open("foo.txt").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    println!("{}", contents);
+}
