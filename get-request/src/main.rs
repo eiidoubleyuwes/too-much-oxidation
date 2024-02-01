01a33973fc45ec3abb517017fe2d15f89614d404
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-02-01 17:43:25 +0300

    Feat:Done with first usese

--- get-request/src/main.rs
+++ get-request/src/main.rs
@@ -7,5 +7,7 @@ fn main() {
     let mut request = reqwest::get("https://httpbin.org/ip")?
         .expect("Couldn't make request");
     let mut body = String::new();
-   
+    request.read_to_string(&mut body)?;
+    println!("{}", body);
+    Ok(())
 }
