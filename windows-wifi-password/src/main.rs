165ada863ee66fa4ab2726902cfbc9338153ff64
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-02-02 22:14:09 +0300

    Feat:Added first bit of functionality

--- windows-wifi-password/src/main.rs
+++ windows-wifi-password/src/main.rs
@@ -1,3 +1,20 @@
+//Simple project to extract wifi password from windows
+use windows ::{
+    core:: {GUID, HSTRING,PCWSTR, PCWSTR},
+    Win32::{
+        Foundation::{ERROR_SUCCESS, HANDLE, INVALID_HANDLE_VALUE, WIN32_ERROR},
+        NetworkManagement::WiFi{
+            WlanOpenHandle,
+            WlanCloseHandle,
+            WlanEnumInterfaces,
+            WlanFreeMemory,
+            WLAN_INTERFACE_INFO_LIST,
+            WLAN_INTERFACE_INFO,
+            WLAN_INTERFACE_STATE,
+        
+    },
+},
+};
 fn main() {
     println!("Hello, world!");
 }
