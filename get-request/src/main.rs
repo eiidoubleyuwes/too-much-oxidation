531777a1706da5407f6f763c549f95b13680f4e4
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-02-02 22:05:12 +0300

    Feat:Bug Fixes

--- get-request/src/main.rs
+++ get-request/src/main.rs
@@ -1,8 +1,6 @@
 use error_chain::error_chain;
 use std::io::Read;
 
-error_chain 
-
 fn main() {
     let mut request = reqwest::get("https://httpbin.org/ip")?
         .expect("Couldn't make request");
