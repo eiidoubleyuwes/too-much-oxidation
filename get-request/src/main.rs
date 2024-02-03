ad51fc29004f4f6d2f94bc86c9153ed9f31f0047
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-02-03 22:54:50 +0300

    Feat:Properly learning slint
    I have learnt how to import the template from their github

--- get-request/src/main.rs
+++ get-request/src/main.rs
@@ -2,7 +2,7 @@ use error_chain::error_chain;
 use std::io::Read;
 
 fn main() {
-    let mut request = reqwest::get("https://httpbin.org/ip")?
+    let mut request = reqwest::get("https://httpbin.org/ip")
         .expect("Couldn't make request");
     let mut body = String::new();
     request.read_to_string(&mut body)?;
