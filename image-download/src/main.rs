3f86d9d2472d09411f0b5048b227ebc710ace433
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-02-08 16:31:37 +0300

    Feat:Added error handling
    This code will handle errors from both the file creation side and the https call

--- image-download/src/main.rs
+++ image-download/src/main.rs
@@ -2,3 +2,10 @@ use error_chain::error_chain;
 use std::fs::File;
 use std::io::copy;
 use tempfile::Builder;
+//To handle all the errors we eill make from file creation to HTTP reqwest
+error_chain! {
+    foreign_links {
+        Io(std::io::Error);
+        HttpRequest(reqwest::Error);
+    }
+}
