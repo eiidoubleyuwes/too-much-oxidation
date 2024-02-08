6c7bdc2fdbcbc99dd35a1e4a1eedaf1add3d5442
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-02-08 16:39:03 +0300

    Feat:Added ability for user to input their own image link
    This is cool because now it makes it such that the app is now more interactive

--- image-download/src/main.rs
+++ image-download/src/main.rs
@@ -2,6 +2,7 @@ use error_chain::error_chain;
 use std::fs::File;
 use std::io::copy;
 use tempfile::Builder;
+use std::io;
 //To handle all the errors we eill make from file creation to HTTP reqwest
 error_chain! {
     foreign_links {
@@ -11,8 +12,12 @@ error_chain! {
 }
 #[tokio::main] // for the async func
 async fn main() -> Result<()> {
+    println!("Enter the URL of the image you want to download: ");
+    let mut target = String::new();
+    io::stdin().read_line(&mut target)?;
+    let target = target.trim(); // remove the trailing newline
     let tm_dir = Builder::new().prefix("Baraka'sdownloader").tempdir()?; // create a temp directory
-    let response = reqwest::get("").await?;
+    let response = reqwest::get(target).await?;
    
     
 }
