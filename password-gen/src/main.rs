5729c4fcdd72a7dff442d836a6f30e95f5b86414
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-02-16 19:20:05 +0300

    Feat:Many updates
    Added dependancies and I have been able to add simple first lines of code

--- password-gen/src/main.rs
+++ password-gen/src/main.rs
@@ -4,3 +4,20 @@
 //The password will be printed to the console
 //The password will be saved to a file called "password.txt"
 
+use rand::Rng;
+use std::io::Write;
+
+fn main() {
+    let password_length = 20; // Set the desired password length
+    let mut password = String::new();
+
+    for _ in 0..password_length {
+        let random_char = rand::thread_rng().gen_range(33..127);
+        password.push(random_char as char);
+    }
+
+    println!("Generated password: {}", password);
+
+    let mut file = std::fs::File::create("password.txt").unwrap();
+    file.write_all(password.as_bytes()).unwrap();
+}
