e91b8820360a0f4b78a53e1d7ce878128e15e5e0
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-02-16 19:18:48 +0300

    Feat:First lines
    This is just a simple description of the project

--- password-gen/src/main.rs
+++ password-gen/src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("Hello, world!");
-}
+//This simple program is for a simple password generator in Rust
+//It will generate a random password of a given length
+//The password will contain a mix of uppercase, lowercase, numbers and special characters
+//The password will be printed to the console
+//The password will be saved to a file called "password.txt"
+
